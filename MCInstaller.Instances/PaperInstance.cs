ee6f1325bbe68165abc3fbd0a653dfe31949ace8
te9c <roman.te9c@gmail.com>
2023-06-23 18:37:08 +0500

    Add realization for PaperInstance class

--- MCInstaller.Instances/PaperInstance.cs
+++ MCInstaller.Instances/PaperInstance.cs
@@ -1,24 +1,11 @@
 using MCInstaller.Utilities;
-using MCInstaller.Core.Exceptions;
 
 namespace MCInstaller.Instances
 {
-    public class PaperInstance : IServerInstance
+    public class PaperInstance : VanillaInstance, IServerInstance
     {
-        public JarReference Jar { get; init; }
-        public JavaReference Java { get; init; }
-        public string WorkingDir { get; init; }
-
-        public PaperInstance(JarReference jar, JavaReference java, string workingDir)
+        public PaperInstance(JarReference jar, JavaReference java, string workingDir) : base(jar, java, workingDir)
         {
-            Jar = jar;
-            Java = java;
-            WorkingDir = workingDir;
-        }
-
-        public Task Init()
-        {
-            throw new TodoException("Init method in PaperInstance");
         }
     }
 }
