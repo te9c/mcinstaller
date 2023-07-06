22e806ecc0ab4a08ec2a63f9cdb0abd84326ca1c
te9c <roman.te9c@gmail.com>
2023-07-07 01:56:15 +0500

    Improve minecraft version parsing.

--- MCInstaller.Jar/MinecraftVersion.cs
+++ MCInstaller.Jar/MinecraftVersion.cs
@@ -1,4 +1,6 @@
 using MCInstaller.Core.Exceptions;
+using System.Text.RegularExpressions;
+using System.Li
 
 namespace MCInstaller.Jar
 {
@@ -25,18 +27,26 @@ namespace MCInstaller.Jar
 
         public static MinecraftVersion Parse(string version)
         {
-            string[] vers = version.Split('.');
-
-            if (vers.Count() < 2 || vers.Count() > 3)
-                throw new ParseException($"version {version} is in wrong format");
-
-            int major = Int32.Parse(vers[0]);
-            int minor = Int32.Parse(vers[1]);
+            int major = 1;
+            int minor = 0;
             int patch = 0;
-            if (vers.Count() == 3)
-                patch = Int32.Parse(vers[2]);
 
-            return new MinecraftVersion(major, minor, patch);
+            var pattern = new Regex(@"^1{1}\.(?<minor>\d{1,2})(\.(?<patch>\d{1}))?$");
+
+            if (pattern.IsMatch(version))
+            {
+                var match = pattern.Match(version);
+
+                minor = Int32.Parse(match.Groups["minor"].Value);
+                if (match.Groups["patch"].Success)
+                {
+                    patch = Int32.Parse(match.Groups["patch"].Value);
+                }
+
+                return new MinecraftVersion(major, minor, patch);
+            }
+
+            throw new ParseException($"Can't parse minecraft version: {version}.");
         }
 
         public static bool TryParse(string version, out MinecraftVersion? mcversion)
